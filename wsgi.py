72a78b5d2ca57549fe41f16d01f671c2561c79a0
Frank Agerholm <frank@agerholm.de>
2020-08-19 21:27:47 +0200

    Initial import
    - API call: /leapyear/<int:i_year>

---
+++ wsgi.py
@@ -0,0 +1,20 @@
+import calendar
+from flask import Flask, jsonify
+import socket
+
+
+application = Flask(__name__)
+
+@application.route("/")
+def hello():
+    return "Hello World!"
+
+@application.route("/leapyear/<int:i_year>")
+def leapyear( i_year ):
+    ret_leap = calendar.isleap(i_year)
+    ret_hostname = socket.gethostname()
+
+    return jsonify( { 'isleapyear': ret_leap, 'worker': ret_hostname } )
+
+if __name__ == "__main__":
+    application.run()
